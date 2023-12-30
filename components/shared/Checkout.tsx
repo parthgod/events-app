d3efb2933af9ea6c2f34dfb9da558f7b4cd48f27
parthgod <parthgenius.gps@gmail.com>
2023-12-30 15:59:22 +0530

    added stripe again

--- components/shared/Checkout.tsx
+++ components/shared/Checkout.tsx
@@ -1,7 +1,7 @@
-import { useEffect } from "react";
-import { Button } from "../ui/button";
-import { IEvent } from "@/lib/database/models/event.model";
+import React, { useEffect } from "react";
 import { loadStripe } from "@stripe/stripe-js";
+import { IEvent } from "@/lib/database/models/event.model";
+import { Button } from "../ui/button";
 import { checkoutOrder } from "@/lib/actions/order.actions";
 
 loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY!);
@@ -15,7 +15,7 @@ const Checkout = ({ event, userId }: { event: IEvent; userId: string }) => {
     }
 
     if (query.get("canceled")) {
-      console.log("Order canceled -- continue to shop around and checkout when you are ready.");
+      console.log("Order canceled -- continue to shop around and checkout when you’re ready.");
     }
   }, []);
 
